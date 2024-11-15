81f7a95676e7974af89962bbd2e2d29c46759cd5
Katarina Vucic <kavucic@icloud.com>
2024-11-15 10:58:36 -0500

    restructured git repo

--- sandbox/keyboard.asm
+++ sandbox/keyboard.asm
@@ -7,7 +7,10 @@
     .data
 ADDR_KBRD:
     .word 0xffff0000
-
+char_a: .asciiz "a"
+char_d: .asciiz "d"
+char_s: .asciiz "s"
+char_w: .asciiz "w"
     .text
 	.globl main
 
@@ -24,12 +27,38 @@ main:
 keyboard_input:                     # A key is pressed
     lw $a0, 4($t0)                  # Load second word from keyboard
     beq $a0, 0x71, respond_to_Q     # Check if the key q was pressed
-
-    li $v0, 1                       # ask system to print $a0
-    syscall
+    
+    beq $a0, 0x61, respond_to_A     # Check if the key a was pressed
+    beq $a0, 0x64, respond_to_D     # Check if the key d was pressed
+    beq $a0, 0x73, respond_to_S     # Check if the key s was pressed
+    beq $a0, 0x77, respond_to_W     # Check if the key w was pressed
 
     b main
 
 respond_to_Q:
 	li $v0, 10                      # Quit gracefully
 	syscall
+
+respond_to_A:
+    li $v0, 4
+    la $a0, char_a
+    syscall                    # Print "a" to console
+    b main 
+    
+respond_to_D:
+    li $v0, 4
+    la $a0, char_d
+    syscall                    # Print "d" to console
+    b main 
+    
+respond_to_S:
+    li $v0, 4
+    la $a0, char_s
+    syscall                    # Print "s" to console
+    b main 
+    
+respond_to_W:
+    li $v0, 4
+    la $a0, char_w
+    syscall                    # Print "w" to console
+    b main 
